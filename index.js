fe240da68a84b8689b8d35699af382f2034b62c8
mmete1 <metemenderes123@gmail.com>
2024-06-23 20:05:41 +0300

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Watching Netflix","Listening to Spotify","Playing GTA VI"];
+const statusMessages = [" Netflix izliyor ","Spotify dinliyor","FiveM oynuyor"];
 
 
 let currentIndex = 0;
